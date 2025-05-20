955f7f16272bc11581f44f7778179ddcc51517d0
Leonardo Serrano Alfaro <leovpc@gmail.com>
2025-05-20 01:00:34 -0600

    Add metadata and constants for improved SEO and application description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,54 @@ import "./globals.css";
 import Link from "next/link";
 import Image from "next/image";
 import { MusicPlayer } from "./(public)/(home)/components/musicPlayer/MusicPlayer";
+import { appDescription, appName, domainUrl } from "@global/constants";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: `%s | ${appName}`,
+    default: `${appName}`,
+  },
+  description: `${appDescription}`,
+  keywords: [
+    "beats",
+    "trap",
+    "dancehall",
+    "afrobeat",
+    "Costa Rica",
+    "reggaeton",
+    "urbano",
+  ],
+  openGraph: {
+    title: `${appName}`,
+    description: `${appDescription}`,
+    url: `${domainUrl}`,
+    siteName: `${appName}`,
+    images: [
+      {
+        url: "/logo_ackee.png",
+        width: 1024,
+        height: 1024,
+        alt: "ackee beats logo",
+      },
+    ],
+    locale: "es_CR",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+};
 
 export default function RootLayout({
   children,
--- src/global/constants.ts
+++ src/global/constants.ts
@@ -1,1 +1,6 @@
 export const whatsappNumber = "50663017707";
+export const appName = "Ackee Beats";
+export const appDescription =
+  "Beats frescos, estilo único, sonido tropical. Trap, R&B, Dancehall, Afrobeat y más. Escucha, compra y sigue creando.";
+export const appUrl = "https://ackeebeats.com";
+export const domainUrl = "ackeebeats.xyz";
